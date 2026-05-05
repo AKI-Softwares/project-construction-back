3721caaab0e82a45ed496d3b06d13d0fbf2abde8
Gustavo <gustavo.savio@outlook.com.br>
2026-05-04 22:44:04 -0300

    feat(add): unique action String and format

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,26 +4,61 @@ const prisma = new PrismaClient()
 
 async function main() {
 
-    const pCreate = await prisma.permission.upset({
-        where: { id: 1 }, update: {},
-        create: { action: 'post:create' },
+    const actions = ['post:create', 'post:update', 'post:read', 'post:delete'];
+
+    await Promise.all(
+        actions.map(action =>
+            prisma.permission.upsert({
+                where: { action },
+                update: {},
+                create: { action }
+            })
+        )
+    );
+
+    await prisma.role.upsert({
+        where: { name: 'ADMIN' },
+        update: {},
+        create: {
+            name: 'ADMIN',
+            permissions: { connect: [{ action: 'post:create' }, { action: 'post:update' }, { action: 'post:read' }, { action: 'post:delete' }] }
+        }
     })
 
-    
-    const pUpdate = await prisma.permission.upset({
-        where: { id: 3 }, update: {},
-        create: { action: 'post:update' },
-    })
-    
-    const pRead = await prisma.permission.upset({
-        where: { id: 4 }, update: {},
-        create: { action: 'post:read' },
+    await prisma.role.upsert({
+        where: { name: 'DIRECTOR' },
+        update: {},
+        create: {
+            name: 'DIRECTOR',
+            permissions: { connect: [{ action: 'post:create' }, { action: 'post:update' }, { action: 'post:read' }, { action: 'post:delete' }] }
+        }
     })
 
-    const pDelete = await prisma.permission.upset({
-        where: { id: 2 }, update: {},
-        create: { action: 'post:delete' },
+    await prisma.role.upsert({
+        where: { name: 'MANAGER' },
+        update: {},
+        create: {
+            name: 'MANAGER',
+            permissions: { connect: [{ action: 'post:create' }, { action: 'post:update' }, { action: 'post:read' }] }
+        }
     })
 
-    
+    await prisma.role.upsert({
+        where: { name: 'USER' },
+        update: {},
+        create: {
+            name: 'USER',
+            permissions: { connect: [{ action: 'post:update' }, { action: 'post:read' }] }
+        }
+    })
 }
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
